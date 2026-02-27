5ee8601986a8174c87f45883f5829d2689b14286
Meraj Alam <167692626+Merajalam2@users.noreply.github.com>
2026-02-27 22:36:49 +0000

    variable , datatype ,

---
+++ 1basic/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1234
+let accountMail = "mail@gmail.com"
+var accountPass = "123"
+accountCity = "Lucknow"
+
+console.log(accountId);
+console.table([accountId,accountMail,accountPass])
---
+++ 1basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";  //treat js as newer version
+//alert(3+3) we ar using node js not browser
+console.log(3+3)
+console.log("Meraj");
+
+let name = "Meraj"
+let age = 18
+let isLoggedIn = true
+//number = 2ki power 53tk
+//bigint
+// string = ""
+//null = standlone value
+//undefined
+//symbol =unique ke liye
+console.log(typeof null);
+ 
